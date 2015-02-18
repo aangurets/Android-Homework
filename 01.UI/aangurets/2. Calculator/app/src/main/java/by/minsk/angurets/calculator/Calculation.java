557340d46215e1d1eef5e700a0c81c6a5c9c3093
Andrei Angurets <andrei.angurets@gmail.com>
2015-02-18 17:36:30 +0300

    Homework Calculator

---
+++ 01.UI/aangurets/2. Calculator/app/src/main/java/by/minsk/angurets/calculator/Calculation.java
@@ -0,0 +1,31 @@
+package by.minsk.angurets.calculator;
+
+
+public class Calculation {
+    public double mOperand1;
+    public double mOperand2;
+
+    public Calculation(double operand1, double operand2) {
+        this.mOperand1 = operand1;
+        this.mOperand2 = operand2;
+    }
+
+    public double sum() {
+        return mOperand1 + mOperand2;
+    }
+
+    public double subtraction() {
+        return mOperand1 - mOperand2;
+    }
+
+    public double division() {
+        if (mOperand1 == 0) {
+            throw new IllegalArgumentException();
+        } else return mOperand1 / mOperand2;
+    }
+
+    public double multiplication() {
+        return mOperand1 * mOperand2;
+    }
+
+}
---
+++ 01.UI/aangurets/2. Calculator/app/src/main/java/by/minsk/angurets/calculator/CalculatorActivity.java
@@ -0,0 +1,103 @@
+package by.minsk.angurets.calculator;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CalculatorActivity extends ActionBarActivity {
+
+    EditText mNum1EditText;
+    EditText mNum2EditText;
+    TextView mResult;
+    RadioGroup mRadioGroup;
+    final static String RESULT = "result";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNum1EditText = (EditText) findViewById(R.id.num1);
+        mNum2EditText = (EditText) findViewById(R.id.num2);
+        mResult = (TextView) findViewById(R.id.result);
+        mRadioGroup = (RadioGroup) findViewById(R.id.radio_group);
+
+        findViewById(R.id.compute_button).setOnClickListener(new OnComputingClickListener());
+    }
+
+    private static double getDouble(TextView textView) {
+        CharSequence text = textView.getText();
+        if (TextUtils.isEmpty(text)) {
+            throw new IllegalArgumentException();
+        } else {
+            try {
+                return Double.parseDouble(text.toString());
+            } catch (Exception e) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private class OnComputingClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            try {
+                switch (mRadioGroup.getCheckedRadioButtonId()) {
+                    case View.NO_ID:
+                        Toast.makeText(
+                                CalculatorActivity.this, R.string.operator_not_select, Toast.LENGTH_SHORT)
+                                .show();
+                    case R.id.operator_sum:
+                        result(new Calculation(getDouble(mNum1EditText), getDouble(mNum2EditText))
+                                .sum());
+                        return;
+                    case R.id.operator_subtr:
+                        result(new Calculation(getDouble(mNum1EditText), getDouble(mNum2EditText))
+                                .subtraction());
+                        return;
+                    case R.id.operator_div:
+                        result(new Calculation(getDouble(mNum1EditText), getDouble(mNum2EditText))
+                                .division());
+                        return;
+                    case R.id.operator_mult:
+                        result(new Calculation(getDouble(mNum1EditText), getDouble(mNum2EditText))
+                                .multiplication());
+                        return;
+                    default:
+                        throw new RuntimeException("Unknown operator ID");
+                }
+            } catch (IllegalArgumentException e) {
+                if (!TextUtils.isEmpty(mResult.getText())) {
+                }
+                Toast.makeText(
+                        CalculatorActivity.this, R.string.incorrect_operand, Toast.LENGTH_SHORT
+                ).show();
+            }
+        }
+    }
+
+    public void result(double doubleResult) {
+        String mStringResult;
+        mStringResult = getString(R.string.result_format, doubleResult);
+        mResult.setText(mStringResult);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putCharSequence(RESULT, mResult.getText());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mResult.setText(savedInstanceState.getCharSequence(RESULT));
+    }
+}
+
