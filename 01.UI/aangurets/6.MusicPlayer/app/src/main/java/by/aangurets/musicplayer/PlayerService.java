e80d9f320f74154ea3a7a2a715fb7869c0318dfd
Andrei Angurets <andrei.angurets@gmail.com>
2015-03-26 15:47:11 +0300

    Music player with StateListDrawable

---
+++ 01.UI/aangurets/6.MusicPlayer/app/src/main/java/by/aangurets/musicplayer/PlayerService.java
@@ -0,0 +1,83 @@
+package by.aangurets.musicplayer;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+
+import java.io.IOException;
+
+public class PlayerService extends IntentService {
+
+    public final static String ACTION_PLAY = "by.aangurets.musicplayer.ACTION_PLAY";
+    public final static String ACTION_PAUSE = "by.aangurets.musicplayer.ACTION_PAUSE";
+    public final static String ACTION_STOP = "by.aangurets.musicplayer.ACTION_STOP";
+    private static MediaPlayer mMediaPlayer;
+
+    public PlayerService() {
+        super(PlayerService.class.getSimpleName());
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (mMediaPlayer == null) {
+            try {
+                initialization();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void initialization() throws IOException {
+        mMediaPlayer = new MediaPlayer();
+        AssetFileDescriptor afd = getAssets().openFd("tiesto.mp3");
+        mMediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.prepare();
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        switch (intent.getAction()) {
+            case ACTION_PLAY:
+                play();
+                break;
+
+            case ACTION_STOP:
+                stop();
+                break;
+
+            case ACTION_PAUSE:
+                pause();
+                break;
+
+            default:
+                throw new IllegalArgumentException(
+                        "Unknown action '" + intent.getAction() + "'.");
+        }
+    }
+
+    private void play() {
+        if (!mMediaPlayer.isPlaying()) {
+            mMediaPlayer.start();
+        }
+    }
+
+    private void pause() {
+        if (mMediaPlayer.isPlaying()) {
+            mMediaPlayer.pause();
+        }
+    }
+
+    private void stop() {
+        if (mMediaPlayer.isPlaying()) {
+            mMediaPlayer.stop();
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+}
